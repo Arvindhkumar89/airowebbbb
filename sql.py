49f5c0d5ec194c05cc70c7ffb81858f9d856cf05
karvindh2002 <karvindh2002@gmail.com>
2024-08-21 23:06:57 +0530

    push

--- sql.py
+++ sql.py
@@ -1,6 +1,6 @@
 import sqlite3
 sqliteConnection = sqlite3.connect('instance\data.db')
-sql_query = """SELECT * FROM user;"""
+sql_query = """SELECT * FROM admin;"""
 cursor = sqliteConnection.cursor()
 cursor.execute(sql_query)
 print(cursor.fetchall())
