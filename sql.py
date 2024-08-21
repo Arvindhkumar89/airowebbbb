924d400de1c30ed3d99f8ae3a5e2a43011657744
karvindh2002 <karvindh2002@gmail.com>
2024-08-21 23:09:23 +0530

    push

--- sql.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-sqliteConnection = sqlite3.connect('instance\data.db')
-sql_query = """SELECT * FROM admin;"""
-cursor = sqliteConnection.cursor()
-cursor.execute(sql_query)
-print(cursor.fetchall())
